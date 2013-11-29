2d0e453e22147625840bc5e0210fdedcd8392054
unknown <you@example.com>
2013-11-29 13:45:06 +0100

    even bijgewerkt

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('SERVERNAME', 'localhost');
+	define('USERNAME', 'root');
+	define('PASSWORD', '');
+	define('DATABASENAME', 'am1b_fotosjaak');
+	date_default_timezone_set("Europe/Amsterdam");
+?>
